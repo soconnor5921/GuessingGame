7849f01a032c042a5eb4ef4bad0bf27c3ca14b84
soconnor5921 <soconnor5921@bths.edu>
2018-09-28 10:15:11 -0400

    End of Class Update 9/28

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,18 +9,23 @@ public class Main
 
     public static void main(String[] args)
     {
-        System.out.println("Type a number from 0-10");
-        Scanner sc = new Scanner(System.in);
-        int ans = sc.nextInt();
+        System.out.println("Type your name");
+        Scanner sc1 = new Scanner(System.in);
+        String playerName = sc1.nextLine();
+
+
+        System.out.println("Hello " + playerName + "! type a number from 0-10");
+        Scanner sc2 = new Scanner(System.in);
+        int ans = sc2.nextInt();
         int aiNum = generateNum();
 
         while(ans != aiNum)
         {
-            System.out.println("Wrong answer, try again");
-            ans = sc.nextInt();
+            System.out.println("Wrong answer, " + playerName + ", try again");
+            ans = sc2.nextInt();
 
         }
-        System.out.println("Correct!");
+        System.out.println("Correct! Good job " + playerName + "!");
 
     }
 }
